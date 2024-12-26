e674e7c2f04065f8560905be2af0562b7d69d58f
GitUserMichael <Saveliev.mix@mail.ru>
2024-12-26 22:40:16 +0300

    Friend functions

--- EX126/EX126.cpp
+++ EX126/EX126.cpp
@@ -1,51 +1,7 @@
 ﻿#include <iostream>
-class Vector3D;
+#include "Vector3D.h"
+#include "Point3D.h"
 
-class Point3D
-{
-private:
-	double m_x, m_y, m_z;
-
-public:
-	Point3D(double x = 0.0, double y = 0.0, double z = 0.0)
-		: m_x(x), m_y(y), m_z(z)
-	{
-
-	}
-	void print()
-	{
-		std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
-	}
-	void moveByVector(const Vector3D& v);
-};
-
-class Vector3D
-{
-private:
-	double m_x, m_y, m_z;
-
-public:
-	Vector3D(double x = 0.0, double y = 0.0, double z = 0.0)
-		: m_x(x), m_y(y), m_z(z)
-	{
-
-	}
-
-	void print()
-	{
-		std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
-	}
-	friend void Point3D::moveByVector(const Vector3D& v);
-};
-
-
-void Point3D::moveByVector(const Vector3D& v)
-{
-	// Реализуйте эту функцию как дружественную классу Vector3D
-	m_x += v.m_x;
-	m_y += v.m_y;
-	m_z += v.m_z;
-}
 
 int main()
 {
